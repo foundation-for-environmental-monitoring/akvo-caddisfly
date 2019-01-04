190848815269ef75f752a829b84adcfff231f54f
nhternup <nhternup@users.noreply.github.com>
2019-01-04 07:26:39 +0530

    [#35] Add print button, Add timeout for web page load

--- caddisfly-app/app/src/main/java/org/akvo/caddisfly/ui/RecommendActivity.java
+++ caddisfly-app/app/src/main/java/org/akvo/caddisfly/ui/RecommendActivity.java
@@ -6,16 +6,15 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
-import android.print.PrintJob;
 import android.print.PrintManager;
 import android.util.SparseArray;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
@@ -32,7 +31,6 @@ import org.akvo.caddisfly.model.TestInfo;
 import org.akvo.caddisfly.util.AssetsManager;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Locale;
@@ -42,15 +40,15 @@ import androidx.databinding.DataBindingUtil;
 
 public class RecommendActivity extends BaseActivity {
 
-    public static final String DATE_FORMAT = "dd MMM yyyy HH:mm";
-    private static ArrayList<PrintJob> mPrintJobs = new ArrayList<>();
-    final Activity activity = this;
-    WebView webView;
-    TestInfo testInfo;
-    RecommendationInfo recommendationInfo = new RecommendationInfo();
-    ActivityRecommendBinding b;
-    String printTemplate;
-    String date;
+    private static final String DATE_FORMAT = "dd MMM yyyy HH:mm";
+    private static final String url = "https://soilhealth.dac.gov.in/calculator/calculator";
+
+    private final Activity activity = this;
+    private final RecommendationInfo recommendationInfo = new RecommendationInfo();
+    private TestInfo testInfo;
+    private ActivityRecommendBinding b;
+    private String printTemplate;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,73 +75,32 @@ public class RecommendActivity extends BaseActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_print, menu);
-        return true;
-    }
-
-    public void onPrint(MenuItem item) {
-        doWebViewPrint();
-    }
-
-    private void doWebViewPrint() {
-        WebView printWebView = new WebView(this);
-        printWebView.setWebViewClient(new WebViewClient() {
-
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
-            }
-
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                createWebPrintJob(view);
-            }
-        });
-
-        printWebView.loadDataWithBaseURL(null, printTemplate, "text/HTML", "UTF-8", null);
-
-    }
-
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void createWebPrintJob(WebView webView) {
 
-        // Get a PrintManager instance
         PrintManager printManager = (PrintManager) this
                 .getSystemService(Context.PRINT_SERVICE);
 
         String jobName = "Fertilizer Recommendation - " + date;
 
-        // Get a print adapter instance
         PrintDocumentAdapter printAdapter = webView.createPrintDocumentAdapter(jobName);
 
 //        PrintAttributes attrib = new PrintAttributes.Builder()
 //                .setMediaSize(PrintAttributes.MediaSize.UNKNOWN_LANDSCAPE)
 //                . build();
 
-        // Create a print job with name and adapter instance
-        PrintJob printJob = printManager.print(jobName, printAdapter,
-                new PrintAttributes.Builder().build());
-
-        // Save the job object for later status checking
-        mPrintJobs.add(printJob);
+        printManager.print(jobName, printAdapter, new PrintAttributes.Builder().build());
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     private void getRecommendation() {
-
-        Intent resultIntent = new Intent();
-
-        String url = "https://soilhealth.dac.gov.in/calculator/calculator";
 
         ProgressDialog pd = new ProgressDialog(this);
         pd.setMessage(getString(R.string.just_a_moment));
         pd.setCancelable(false);
         pd.show();
 
-        webView = new WebView(this);
+        WebView webView = new WebView(this);
 
         webView.getSettings().setJavaScriptEnabled(true);
 
@@ -200,82 +157,131 @@ public class RecommendActivity extends BaseActivity {
                 "})();";
 
         webView.setWebViewClient(new WebViewClient() {
-            @SuppressWarnings("deprecation")
+
+            boolean timeout = true;
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                Runnable run = () -> {
+                    if (timeout) {
+                        pd.dismiss();
+                        webView.stopLoading();
+                        finish();
+                        Toast.makeText(activity, "Connection Timed out", Toast.LENGTH_SHORT).show();
+                    }
+                };
+                Handler myHandler = new Handler(Looper.myLooper());
+                myHandler.postDelayed(run, 20000);
+            }
+
             @Override
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
                 Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+                timeout = false;
                 pd.dismiss();
+                finish();
             }
 
             @TargetApi(android.os.Build.VERSION_CODES.M)
             @Override
-            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError error) {
                 // Redirect to deprecated method, so you can use it in all SDK versions
-                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+                onReceivedError(view, error.getErrorCode(), error.getDescription().toString(), req.getUrl().toString());
             }
 
             public void onPageFinished(WebView view, String url) {
                 view.evaluateJavascript(js, s -> {
 
                     String[] values = s.replace("\"", "").split(",");
-
-                    SparseArray<String> results = new SparseArray<>();
-                    recommendationInfo.state = values[0];
-                    recommendationInfo.district = values[1];
-                    recommendationInfo.crop = values[2];
-
-                    preparePrintDocument();
-
-                    for (int i = 0; i < testInfo.getResults().size(); i++) {
-                        Result result = testInfo.getResults().get(i);
-                        resultIntent.putExtra(result.getName().replace(" ", "_")
-                                + testInfo.getResultSuffix(), values[i + 3]);
-
-                        results.append(result.getId(), result.getResult());
-
-                        printTemplate = printTemplate.replace("{Value" + i + "}", values[i + 3]);
+                    if (values.length > 10) {
+                        timeout = false;
+                        displayResult(values);
                     }
-
-                    recommendationInfo.values = Arrays.copyOfRange(values, 3, values.length);
-
-                    b.setInfo(recommendationInfo);
-
-                    setResult(Activity.RESULT_OK, resultIntent);
                 });
-                (new Handler()).postDelayed(pd::dismiss, 3000);
+                (new Handler()).postDelayed(pd::dismiss, 4000);
             }
         });
 
         webView.loadUrl(url);
     }
 
+    private void displayResult(String[] values) {
+
+        Intent resultIntent = new Intent();
+
+        SparseArray<String> results = new SparseArray<>();
+        recommendationInfo.state = values[0];
+        recommendationInfo.district = values[1];
+        recommendationInfo.crop = values[2];
+
+        for (int i = 0; i < testInfo.getResults().size(); i++) {
+            Result result = testInfo.getResults().get(i);
+            resultIntent.putExtra(result.getName().replace(" ", "_")
+                    + testInfo.getResultSuffix(), values[i + 3]);
+
+            results.append(result.getId(), result.getResult());
+
+            printTemplate = printTemplate.replace("#Value" + i + "#", values[i + 3]);
+        }
+
+        preparePrintDocument();
+
+        recommendationInfo.values = Arrays.copyOfRange(values, 3, values.length);
+
+        b.setInfo(recommendationInfo);
+
+        setResult(Activity.RESULT_OK, resultIntent);
+    }
+
     private void preparePrintDocument() {
         date = new SimpleDateFormat(DATE_FORMAT, Locale.US).format(Calendar.getInstance().getTime());
-        printTemplate = printTemplate.replace("{Date}", date);
-        printTemplate = printTemplate.replace("{FarmerName}", recommendationInfo.farmerName);
-        printTemplate = printTemplate.replace("{PhoneNumber}", recommendationInfo.phoneNumber);
-        printTemplate = printTemplate.replace("{VillageName}", recommendationInfo.villageName);
-        printTemplate = printTemplate.replace("{State}", recommendationInfo.state);
-        printTemplate = printTemplate.replace("{District}", recommendationInfo.district);
-        printTemplate = printTemplate.replace("{SampleNumber}", recommendationInfo.sampleNumber);
-        printTemplate = printTemplate.replace("{Crop}", recommendationInfo.crop);
+        printTemplate = printTemplate.replace("#Date#", date);
+        printTemplate = printTemplate.replace("#FarmerName#", recommendationInfo.farmerName);
+        printTemplate = printTemplate.replace("#PhoneNumber#", recommendationInfo.phoneNumber);
+        printTemplate = printTemplate.replace("#VillageName#", recommendationInfo.villageName);
+        printTemplate = printTemplate.replace("#State#", recommendationInfo.state);
+        printTemplate = printTemplate.replace("#District#", recommendationInfo.district);
+        printTemplate = printTemplate.replace("#SampleNumber#", recommendationInfo.sampleNumber);
+        printTemplate = printTemplate.replace("#Crop#", recommendationInfo.crop);
 
         if (recommendationInfo.geoLocation != null && !recommendationInfo.geoLocation.isEmpty()) {
             String[] geoValues = recommendationInfo.geoLocation.split(" ");
             for (int i = 0; i < geoValues.length; i++) {
-                printTemplate = printTemplate.replace("{Geo" + i + "}", geoValues[i]);
+                // Also show unit (m) for last two values
+                printTemplate = printTemplate.replace("#Geo" + i + "#",
+                        i > 1 ? geoValues[i] + "m" : geoValues[i]);
             }
         }
 
-        printTemplate = printTemplate.replace("{Nitrogen}", recommendationInfo.nitrogenResult);
-        printTemplate = printTemplate.replace("{Phosphorus}", recommendationInfo.phosphorusResult);
-        printTemplate = printTemplate.replace("{Potassium}", recommendationInfo.potassiumResult);
+        printTemplate = printTemplate.replace("#Nitrogen#", recommendationInfo.nitrogenResult);
+        printTemplate = printTemplate.replace("#Phosphorus#", recommendationInfo.phosphorusResult);
+        printTemplate = printTemplate.replace("#Potassium#", recommendationInfo.potassiumResult);
+
+        printTemplate = printTemplate.replaceAll("#.*?#", "");
+    }
+
+    public void onPrintClick(View view) {
+        WebView printWebView = new WebView(this);
+        printWebView.setWebViewClient(new WebViewClient() {
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                createWebPrintJob(view);
+            }
+        });
+
+        printWebView.loadDataWithBaseURL(null, printTemplate, "text/HTML", "UTF-8", null);
     }
 
     public void onSaveClick(View view) {
         finish();
     }
-
 
     private String getStringExtra(String key) {
         return getStringExtra(key, "");
