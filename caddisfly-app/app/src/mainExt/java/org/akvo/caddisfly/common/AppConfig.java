7418983da0a830018010afb6ff09c5f911fe3046
nhternup <nhternup@users.noreply.github.com>
2019-04-23 14:39:54 +0530

    Update version details

--- caddisfly-app/app/src/mainExt/java/org/akvo/caddisfly/common/AppConfig.java
+++ caddisfly-app/app/src/mainExt/java/org/akvo/caddisfly/common/AppConfig.java
@@ -33,7 +33,7 @@ public final class AppConfig {
      * Date on which the app version will expire.
      * This is to ensure that installs from apk meant for testing only are not used for too long.
      */
-    public static final boolean APP_EXPIRY = true;
+    public static final boolean APP_EXPIRY = false;
     public static final int APP_EXPIRY_DAY = 1;
     public static final int APP_EXPIRY_MONTH = 4;
     public static final int APP_EXPIRY_YEAR = 2019;
