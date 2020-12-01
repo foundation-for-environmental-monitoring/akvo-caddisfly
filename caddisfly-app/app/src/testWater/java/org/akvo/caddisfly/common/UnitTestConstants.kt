589c1d444f19e60fb7159e822c4a39553b7191f0
nhternup <nhternup@users.noreply.github.com>
2020-12-01 06:49:01 +0530

    [#74] Add Zinc water test parameter

--- caddisfly-app/app/src/testWater/java/org/akvo/caddisfly/common/UnitTestConstants.kt
+++ caddisfly-app/app/src/testWater/java/org/akvo/caddisfly/common/UnitTestConstants.kt
@@ -4,9 +4,9 @@ import org.akvo.caddisfly.model.TestSampleType
 
 object UnitTestConstants {
     const val STRIP_TESTS_COUNT = 2
-    const val CUVETTE_TESTS_COUNT = 19
+    const val CUVETTE_TESTS_COUNT = 20
     const val CUVETTE_TEST_ID_1 = "222ca914-3036-4a2e-a59d-d85948d69461"
-    val SAMPLE_TYPE: TestSampleType? = TestSampleType.WATER
+    val SAMPLE_TYPE: TestSampleType = TestSampleType.WATER
     const val STRIP_TEST_NAME = "Arsenic (0 – 500)"
     const val CUVETTE_TEST_NAME_1 = "Alkalinity"
     const val CUVETTE_TEST_NAME_2 = "Aluminum"
