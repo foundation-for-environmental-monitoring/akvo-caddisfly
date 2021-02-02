762a177ffe858e52a1e511a41a88888954fd14a4
nhternup <nhternup@users.noreply.github.com>
2021-02-02 16:11:23 +0530

    [#80] Update sulfite calc formula

--- caddisfly-app/app/src/testWater/java/org/akvo/caddisfly/common/UnitTestConstants.kt
+++ caddisfly-app/app/src/testWater/java/org/akvo/caddisfly/common/UnitTestConstants.kt
@@ -3,7 +3,7 @@ package org.akvo.caddisfly.common
 import org.akvo.caddisfly.model.TestSampleType
 
 object UnitTestConstants {
-    const val CUVETTE_TESTS_COUNT = 23
+    const val CUVETTE_TESTS_COUNT = 24
     const val CUVETTE_TEST_ID_1 = "222ca914-3036-4a2e-a59d-d85948d69461"
     val SAMPLE_TYPE: TestSampleType = TestSampleType.WATER
     const val CUVETTE_TEST_NAME_1 = "Alkalinity"
